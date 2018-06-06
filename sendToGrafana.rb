97bc207de0e41bd02580870f368015db246325c2
jake <jake@voidnet.com>
2018-06-05 19:23:31 -0500

    stuff

--- sendToGrafana.rb
+++ sendToGrafana.rb
@@ -1,6 +1,11 @@
+require 'rest-client'
+require 'json'
+
+
 class send_to_Grafana
-  def apicall
-
+  def apicall(url='http://10.0.1.81:8086/write?db=mydb', payload)
+    puts("Sending API call to #{url}")
+    responce = RestClient.post(url, payload)
+    return responce
   end
-
 end
