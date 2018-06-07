95ea8296a7ca0e462c936202d13c9c84006a788c
jake <jake@voidnet.com>
2018-06-06 19:54:23 -0500

    stuff

--- lib/sendToGrafana.rb
+++ lib/sendToGrafana.rb
@@ -5,12 +5,21 @@ require 'json'
 
 
 class Send_To_Grafana
-  def apicall(payload, url='http://10.0.1.81:8086/write?db=mydb')
+  def initialize()
+    @url = 'http://10.0.1.81:8086/write?db=mydb'
+    @payload = payload
+  end
+
+  def apicall(payload)
     puts("Sending API call to #{url}")
-    responce = RestClient.post(url, payload)
+    responce = RestClient.post(@url, @payload)
     return responce
+  end
+
+  def failure
+    puts "Failed to make API call to #{@url}"
   end
 end
 
-call = Send_To_Grafana.new()
-call.apicall(payload, url)
+call = Send_To_Grafana.new(payload)
+call.apicall(@payload)
