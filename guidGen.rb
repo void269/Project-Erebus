0111cd3aa16078d62c8dd435aa1d8faa6d2ec116
jake <jake@voidnet.com>
2018-06-16 15:40:59 -0500

    building code

--- guidGen.rb
+++ guidGen.rb
@@ -1,21 +1,21 @@
-slavefile  = File.open("/etc/Project-Erebus/slave", 'w')
+@slavefile  = File.open("/etc/Project-Erebus/slave", 'w')
 
-if slavefile
+if @slavefile
   puts "This is a Slave"
   puts "file size is: #{slavefile.size}"
-  if slavefile.size == 0
+  if @slavefile.size == 0
     puts "Generating UUID for first run!"
     require 'securerandom'
     require 'socket'
 
     ip_addrs = Socket.ip_address_list
-    allips = ip_addrs.reject {|i| i.ip_address =~ /127/ || i.ip_address =~ /:/}
-    ip = allips[0].ip_address
+    all_ips = ip_addrs.reject {|i| i.ip_address =~ /127/ || i.ip_address =~ /:/}
+    ip = all_ips[0].ip_address
 
     uuid = SecureRandom.uuid
-    slavefile.write("#{uuid},#{ip}")
+    @slavefile.write("#{uuid},#{ip}")
   else
-    uuid = slavefile.split(',')[0]
+    uuid = @slavefile.split(',')[0]
     puts "This Slave already has a UUID of #{uuid}"
     puts "Nothing to do, exiting"
   end
