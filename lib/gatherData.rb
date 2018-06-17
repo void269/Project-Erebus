5def3d7f0774870b164ee118bbe23e6676b69a50
jake <jake@voidnet.com>
2018-06-17 18:24:08 -0500

    building code

--- lib/gatherData.rb
+++ lib/gatherData.rb
@@ -1,8 +1,7 @@
 #!/usr/bin/env ruby
 
-
 class Gatherdata
-  def getdata_ina219
+  def Getdata_ina219
     result = exec("python ./return_ina219.py 0.03")
     puts "INA219 return is: #{result}"
   end
