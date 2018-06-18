088de70348e77bea97b8cca5bebabd5a6d43d268
jake <jake@voidnet.com>
2018-06-17 20:56:28 -0500

    building code

--- lib/return_ina219.py
+++ lib/return_ina219.py
@@ -1,12 +1,10 @@
 from ina219 import INA219
 import sys
 
-print ("---")
-print (sys.argv[1])
-print ("---")
+measure = sys.argv[1]
 
 ina = INA219(shunt_ohms=0.1,
-  max_expected_amps = sys.argv[1],
+  max_expected_amps = measure,
   address=0x40)
 
 ina.configure(voltage_range=ina.RANGE_16V,
