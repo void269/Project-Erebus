d0f38f9bdbea2fcab7ba8d8abff94b06f59c3216
jake <jake@voidnet.com>
2018-06-18 19:53:56 -0500

    building code

--- Sinatra/slave.rb
+++ Sinatra/slave.rb
@@ -3,8 +3,9 @@ require '/etc/Project-Erebus/lib/gatherData.rb'
 
 class MyApp < Sinatra::Base
   get '/' do
-    result = Gatherdata.new
-    result
+    #result = Gatherdata.new
+    #result
+    'testing'
   end
 end
 
