e48e9a59b533fd0973d5b353c286bbc6cfd4b492
jake <jake@voidnet.com>
2018-06-18 20:01:24 -0500

    building code

--- Sinatra/slave.rb
+++ Sinatra/slave.rb
@@ -2,7 +2,7 @@ require 'sinatra/base'
 
 class MyApp < Sinatra::Base
   get '/' do
-    require '/etc/Project-Erebus/lib/gatherData.rb'
+    require '/etc/Project-Erebus/lib/gatherData.rb' #THIS IS THE PROBLEM!!!!!!!!
     result = Gatherdata.new
     result
     'testing'
