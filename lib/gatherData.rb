b40c33a61f6956dd9d43a7d574bb9f3df874ba28
jake <jake@voidnet.com>
2018-06-18 19:31:22 -0500

    building code

--- lib/gatherData.rb
+++ lib/gatherData.rb
@@ -1,10 +1,6 @@
 #!/usr/bin/env ruby
 
 class Gatherdata
-  def initialize(milliamp_measure)
-    @milliamp_measure = milliamp_measure
-  end
-
   def getdata_ina219
     result = exec("python /etc/Project-Erebus/bin/return_ina219.py 0.03")
     puts "INA219 return is: #{result}"
