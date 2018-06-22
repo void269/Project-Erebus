d438025c3b20af200b4865d7a7b75e0190f74a38
jake <jake@voidnet.com>
2018-06-22 17:41:06 -0500

    building code

--- lib/gatherData.rb
+++ lib/gatherData.rb
@@ -6,8 +6,9 @@ class Gatherdata
   end
 
   def getdata_ina219
-    result = exec("python /etc/Project-Erebus/bin/return_ina219.py 0.03")
+    #result = exec("python /etc/Project-Erebus/bin/return_ina219.py 0.03")
     #puts "INA219 return is: #{result}"
-    return result
+    #result
+    return "you got the data!"
   end
 end
