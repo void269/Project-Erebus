cebe2c4f46ac1ae75aae143ace5c723cd20ae0ea
jake <jake@voidnet.com>
2018-06-22 17:54:06 -0500

    building code

--- bin/return_ina219.py
+++ bin/return_ina219.py
@@ -1,19 +1,18 @@
 from ina219 import INA219
 import sys
 
-def main():
-  measure = float(sys.argv[1])
+measure = float(sys.argv[1])
 
-  ina = INA219(shunt_ohms=0.1,
+ina = INA219(shunt_ohms=0.1,
     max_expected_amps = measure,
     address=0x40)
 
-  ina.configure(voltage_range=ina.RANGE_16V,
+ina.configure(voltage_range=ina.RANGE_16V,
     gain=ina.GAIN_AUTO,
     bus_adc=ina.ADC_128SAMP,
     shunt_adc=ina.ADC_128SAMP)
 
-  v = ina.voltage()
-  i = ina.current()
-  p = ina.power()
-  return (v,i,p)
+v = ina.voltage()
+i = ina.current()
+p = ina.power()
+print (v,i,p)
