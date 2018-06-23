c0f204d5e4a6c28a6ee2ec6c5131321ded55dfce
jake <jake@voidnet.com>
2018-06-23 09:12:25 -0500

    building code

--- lib/logging.rb
+++ lib/logging.rb
@@ -3,18 +3,18 @@ require 'fileutils'
 
 class App_Logger
   def initialize
-    @logpath = "/etc/Project-Erebus/logs"
-    @logfilename = "runtime.log"
-    @loglongname = "#{@logpath}/#{@logfilename}"
-    FileUtils.mkdir_p @logpath
+    logpath = "/etc/Project-Erebus/logs"
+    logfilename = "runtime.log"
+    @loglongname = "#{logpath}/#{logfilename}"
+    FileUtils.mkdir_p logpath
     File.new(@loglongname, "w") unless File.exist?(@loglongname)
-    @logfile = File.open("#{@logpath}/#{@logfilename}", "w")
   end
 
   def main(log_level = "INFO", text = nil, app_name = "Erebus")
     time = Time.new.utc.strftime('%m-%d-%y %H:%M:%S.%L%L')
-    @logfile.write("\n")
-    @logfile.write("#{time} -> #{log_level.upcase} - [#{app_name}] #{text}")
-    @logfile.close
+    logfile = File.open(@loglongname, "w")
+    logfile.write("\n")
+    logfile.write("#{time} -> #{log_level.upcase} - [#{app_name}] #{text}")
+    logfile.close
   end
 end
