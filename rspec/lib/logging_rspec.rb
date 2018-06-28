2f3cbefcc37ea16874841932925c827d91508c0b
jake <jake@voidnet.com>
2018-06-28 18:39:08 -0500

    building code

--- rspec/lib/logging_rspec.rb
+++ rspec/lib/logging_rspec.rb
@@ -4,10 +4,11 @@ require "logging"
 
 describe App_Logger do
   it ".should create a new instance of the class" do
-    @logging = App_Logger.new
+    logging = App_Logger.new
   end
 
   it "#should initialze instance variables" do
-    @logging.loglongname.should == "/etc/Project-Erebus/logs/runtime.log"
+    logging = App_Logger.new
+    logging.loglongname.should == "/etc/Project-Erebus/logs/runtime.log"
   end
 end
