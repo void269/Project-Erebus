fa70d4202964d4cecc9b75303f686efd362911a3
jake <jake@voidnet.com>
2018-06-27 20:19:09 -0500

    building code

--- rspec/lib/logging_rspec.rb
+++ rspec/lib/logging_rspec.rb
@@ -1,9 +1,11 @@
+$LOAD_PATH.unshift('/etc/Project-Erebus/lib')
 require "spec_helper"
+require "logging"
 
 describe App_Logger do
   it "#should write to a log file" do
     logging = App_Logger.new #.main(:info, "rspec test")
     logging.logpath.should == "/etc/Project-Erebus/logs"
-    
+
   end
 end
