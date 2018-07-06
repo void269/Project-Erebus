e3eed414807976ca5d5ccbaa463d28c8a86bbb3d
jake <jake@voidnet.com>
2018-07-06 08:53:28 -0500

    building code

--- lib/uuidmgmt.rb
+++ lib/uuidmgmt.rb
@@ -9,39 +9,35 @@ class UUID
     @log.write(:info, "this is a test")
     if File.exist?("/etc/erebus/slave")
       uuid_gen_slave
-    end
-
-    if File.exist?("/etc/erebus/master")
+    elsif File.exist?("/etc/erebus/master")
       uuid_gen_master
+    else
+      @log.write(:error, "missing slave/master file, cannot proceed!")
+      return {:success => false, :state => "missing slave/master file", :output => ""}
     end
   end
 
   def uuid_gen_slave
-    if file_exist?("/etc/erebus/slave")
-      @log.write(:INFO, "slave file exists")
-      if file_empty?("/etc/erebus/slave")
-        @log.write(:INFO, "Starting UUID Gen for slave")
-        ip = get_local_ip
-        @log.write(:INFO, "IP addr found -> #{ip}")
-        uuid = SecureRandom.uuid
-        @log.write(:INFO, "UUID generated -> #{uuid}")
-        @log.write(:INFO, "in uuid buld")
-        @output = "#{uuid},#{ip}"
-        File.open("/etc/erebus/slave", 'w') do |f|
-          f.write(@output)
-          f.close
-        end
-        @log.write(:INFO, "before return")
-        return {:success => true, :state => "new", :output => @output}
-        @log.write(:INFO, "after return")
-      else
-        @log.write(:WARN, "Slave file already contains data, doing nothing!")
-        exist = File.open("/etc/erebus/slave", 'r')
-        return {:success => true, :state => "existing", :output => exist.read}
+    @log.write(:INFO, "slave file exists")
+    if file_empty?("/etc/erebus/slave")
+      @log.write(:INFO, "Starting UUID Gen for slave")
+      ip = get_local_ip
+      @log.write(:INFO, "IP addr found -> #{ip}")
+      uuid = SecureRandom.uuid
+      @log.write(:INFO, "UUID generated -> #{uuid}")
+      @log.write(:INFO, "in uuid buld")
+      @output = "#{uuid},#{ip}"
+      File.open("/etc/erebus/slave", 'w') do |f|
+        f.write(@output)
+        f.close
       end
+      @log.write(:INFO, "before return")
+      return {:success => true, :state => "new", :output => @output}
+      @log.write(:INFO, "after return")
     else
-      @log.write(:error, "missing slave file, cannot proceed!")
-      return {:success => false, :state => "missing slave file", :output => ""}
+      @log.write(:WARN, "Slave file already contains data, doing nothing!")
+      exist = File.open("/etc/erebus/slave", 'r')
+      return {:success => true, :state => "existing", :output => exist.read}
     end
   end
 
