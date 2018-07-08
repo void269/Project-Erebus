09055eeaa05bdd521bdeec247b9a8f02fcac57fa
jake <jake@voidnet.com>
2018-07-08 10:59:21 -0500

    building code

--- lib/manageSlave.rb
+++ lib/manageSlave.rb
@@ -21,7 +21,7 @@ class ManageSlave
     @log.write(:info, "Checking if slave already exists")
     ### generate guid for slave ###
     result = RestClient.post("http://#{ip}/uuidgen", nil)
-    @log.write(:info, "result is: #{result.body.class}")
+    @log.write(:info, "result is: #{result.body}")
     slaveuuid = result['uuid']
     @log.write(:info, "slaveuuid is: #{slaveuuid}")
     existing = File.open(@master).grep(/#{slaveuuid.split(',')[0]}/)[0].chomp
